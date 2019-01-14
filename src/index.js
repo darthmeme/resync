8c99dfdf9868e243b2a9dd889c1b985c4af9213e
Akira Laine <akira_laine@outlook.com>
2019-01-14 15:46:43 +1100

    feature: resync subtitles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
-console.log('Hello World')
+const fs = require('fs')
+const { parse, resync, stringify } = require('subtitle')
+
+console.log('🔄 Resyncing Subtitles...')
+
+/**
+ * Get arguments and file content
+ */
+const [filePath, delay] = process.argv.slice(2)
+const fileContent = fs.readFileSync(filePath, 'utf-8')
+
+/**
+ * Resync subtitles
+ */
+const parsedSubtitles = parse(fileContent)
+const resyncedSubtitles = resync(parsedSubtitles, Number(delay))
+
+/**
+ * Override file
+ */
+fs.writeFileSync(filePath, stringify(resyncedSubtitles))
+
+console.log('✅ Resync completed!')
